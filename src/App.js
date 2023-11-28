97d14495fa2192d0400fa5dbfd5a6258fe87cf74
Aziz4tostaboi <abylovaziz@gmail.com>
2023-11-28 11:12:34 +0600

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import './darkMode.css';
+import "./App.css"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [theme, setTheme] = useState('light');
+    const toggleTheme = () => {
+        if (theme === 'light') {
+            setTheme('dark');
+        } else {
+            setTheme('light');
+        }
+    };
+    useEffect(() => {
+        document.body.className = theme;
+    }, [theme]);
+    return (
+        <div className={`App ${theme}`}>
 
+            <div>
+                <p className="dark-light">dark/light</p>
+                <div className="container">
+                    <input type="checkbox" className="checkbox" id="checkbox" onClick={toggleTheme}/>
+                        <label className="switch" htmlFor="checkbox">
+                            <span className="slider"></span>
+                        </label>
+                </div>
+            </div>
+        </div>
+    );
+}
 export default App;
